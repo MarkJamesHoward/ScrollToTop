2b9f03c80d47fa40866298db37af0b2a00a44fab
MarkHoward <markjameshoward@hotmail.co.uk>
2019-06-24 20:40:33 +0100

    pika!

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { property, LitElement, html, customElement } from "lit-element";
+import { property, LitElement, html, customElement } from "/web_modules/lit-element.js";
 import "@polymer/paper-toast";
 
 /**
