6d2933f155d1ef749d2a06f416e71aa0bf675f28
MarkJamesHoward <markjameshoward@hotmail.co.uk>
2021-01-26 15:08:45 +1300

    types added

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+import { LitElement } from 'https://cdn.skypack.dev/lit-element';
+export declare class ScrollToTop extends LitElement {
+    activatewhen: Number;
+    showToast: Boolean;
+    render(): any;
+    firstUpdated(): void;
+    scrollFunction(): void;
+    topFunction(): void;
+}
